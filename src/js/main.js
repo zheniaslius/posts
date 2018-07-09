4e1aa25e9b8568c81fe34267a91f30712b89afe2
ZheniaFIT <zhen0077@ukr.net>
2018-07-09 14:26:15 +0200

    infinite scroll works

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,18 @@ const postsContainer = document.querySelector('.posts-wrp');
 const tagsContainer = document.querySelector('.tags');
 const searchBtn = document.querySelector('.search');
 const clearBtn = document.querySelector('.clear');
+const search = document.querySelector('#search');
 
 tagsContainer.addEventListener('click', (e) => {
+  let tag = e.target;
   let tagValue = e.target.innerHTML;
-  if (e.target.classList.contains('tag'))
-    e.target.classList.toggle('selected');
+  if (tag.classList.contains('tag')) {
+    tag.classList.toggle('selected');
+  }
 });
 
 clearBtn.addEventListener('click', (e) => {
   const tags = [...document.querySelectorAll('.tag')];
-
 });
 
 const getPosts = async () => {
@@ -26,15 +28,29 @@ const displayPosts = posts => {
   let output = '';
   posts.forEach(post => {
     output += `
-      <h1>${post.title}</h1>
-      <span>${post.description}</span>
-      <img src="${post.image}"></img>
-      <img>${post.image}</img>
-      <span>${post.createdAt}</span>
-      <ul>${post.tags.map(tag => `<li>${tag}</li>`)}</ul>
+      <div class="post">
+        <h1>${post.title}</h1>
+        <span>${post.description}</span>
+        <img src="${post.image}"></img>
+        <img>${post.image}</img>
+        <span>${post.createdAt}</span>
+        <ul>${post.tags.map(tag => `<li>${tag}</li>`)}</ul>
+        <button id="delete-post">Delete</button>
+      </div>
       `;
   });
   postsContainer.innerHTML = output;
+
+  document.addEventListener('scroll', event => {
+    let element = event.target;
+  
+    var scrollTop = element.scrollTop || document.documentElement.scrollTop;
+    if (scrollTop + window.innerHeight >= document.body.offsetHeight) {
+      postsContainer.innerHTML += output;
+    }
+  });
+  
+  return posts;
 }
 
 const displayTags = tags => {
@@ -46,6 +62,7 @@ const displayTags = tags => {
 }
 
 const sortDateByDesc = posts => {
+  localStorage.setItem('sortBy', 'dateDesc');
   let newPosts = [...posts];
   newPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   displayPosts(newPosts);
@@ -54,6 +71,7 @@ const sortDateByDesc = posts => {
 const dateAsc = (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
 
 const sortDateByAsc = posts => {
+  localStorage.setItem('sortBy', 'dateAsc');
   let newPosts = [...posts];
   newPosts.sort(dateAsc);
   displayPosts(newPosts);
@@ -78,6 +96,7 @@ const sameTagsCount = (post) => {
 }
 
 const sortByTags = posts => {
+  localStorage.setItem('sortBy', 'tags');
   let sortedPosts = Array.from(posts);
   sortedPosts.sort((a, b) => {
     dateDiff = sameTagsCount(b) - sameTagsCount(a);
@@ -86,12 +105,43 @@ const sortByTags = posts => {
   displayPosts(sortedPosts);
 }
 
+const handleSearch = (e, posts) => {
+  let searchFor = e.target.value.toLowerCase();
+  displayPosts(posts.filter(post => post.title.toLowerCase().includes(searchFor)));
+}
+
+const handlePostDelete = () => {
+  const deleteBtn = [...document.querySelectorAll('#delete-post')];
+  deleteBtn.forEach(btn => btn.addEventListener('click', e => 
+    e.target.parentElement.style.display = "none")
+  );
+}
+
 const handleMenu = posts => {
   ascending.addEventListener('click', () => sortDateByAsc(posts));
   descending.addEventListener('click', () => sortDateByDesc(posts));
+  search.addEventListener('keyup', e => handleSearch(e, posts));
+  searchBtn.addEventListener('click', () => sortByTags(posts));
 
   displayTags(getUniqueTags(posts));
-  searchBtn.addEventListener('click', () => sortByTags(posts))
 }
 
-getPosts().then(handleMenu);
+const initialRender = posts => {
+  let sortBy = localStorage.getItem('sortBy');
+  switch (sortBy) {
+    case 'tags': 
+      sortByTags(posts);
+      break;
+    case 'dateAsc':
+      sortDateByAsc(posts);
+      break;
+    default:
+      sortDateByDesc(posts);  
+  }
+  return posts;
+}
+
+getPosts()
+  .then(initialRender)
+  .then(handleMenu)
+  .then(handlePostDelete);
